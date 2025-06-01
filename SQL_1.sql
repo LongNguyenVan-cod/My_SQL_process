54d2537780b6a8ea39afa2e6855e2e40eddae604
LongNguyenVan-cod <longnguyenvanfpf@gmail.com>
2025-06-01 16:40:04 +0700

    Update SQL_1.sql

--- SQL_1.sql
+++ SQL_1.sql
@@ -1,30 +1,8 @@
 
--- Tao database va goi database su dung
-create DATABASE MX_practices
-use MX_practices
+create DATABASE Business
+use Business
 
-
--- Lay ra cac thong tin theo mot so dieu kien ap dung
-select
-    so.TerritoryKey,
-    sum(so.SalesAmount) as "Total_Amount",
-    sum(case when year(so.OrderDate)= 2015 then so.SalesAmount else 0 end) as "Sales_Amount_2015",
-    count(distinct so.CustomerKey) as Customercount,
-    count(distinct case when year(so.OrderDate) = 2015 then so.CustomerKey else null end) as "Customer_2015_count",
-    count(distinct so.OrderNumber) as "OrderNumber_count",
-    count(distinct case when year(so.OrderDate) = 2015 then so.OrderNumber else null end) as "OrderNumber_2015_count"
-from [dbo].[SaleOrders] as so
-group by so.TerritoryKey
-order by 1
-
--------------
-select 
-    count(distinct so.CustomerKey) 
-from [dbo].[SaleOrders] as so
-where so.TerritoryKey = 1 and year(so.OrderDate) =  2015
 --------------
-
-
 --Phân loại khách hàng theo RFM
 --R là bao lâu rồi chưa mua hàng
 --F là số đơn
@@ -38,15 +16,15 @@ with tb1 as (
         sum(so.SalesAmount) as M
     from [dbo].[SaleOrders] as so
     group by so.CustomerKey
-    ),
-    tb2 as (
+),
+tb2 as (
     select *,
         PERCENT_RANK() over (order by R) as "R_percent",
         PERCENT_RANK() over (order by F) as "F_percent",
         PERCENT_RANK() over (order by M) as "M_percent"
     from tb1
-    ),
-    tb3 as (
+),
+tb3 as (
     select *,
         case 
             when R_percent < 0.2 then '5'
@@ -70,15 +48,26 @@ with tb1 as (
             when M_percent <= 1 then '5'
         end as M_score
     from tb2
-    ),
-    tb4 as (
+),
+tb4 as (
     select 
         Segment,
         trim([value]) as "RFM_score"
     from [dbo].[RankRFM]
     cross apply string_split(Scores, ',')
-    )
-
+)
+-- , tb5 as (
+--     select 
+--         CustomerKey,
+--         R_score,
+--         F_score,
+--         M_score,
+--         concat(R_score, F_score, M_score) as "RFM_score",
+--         Segment
+--     from tb3 
+--     left join tb4 
+--         on concat(tb3.R_score, tb3.F_score, tb3.M_score) = tb4.RFM_score
+-- )
 select 
     CustomerKey,
     R_score,
@@ -91,17 +80,17 @@ left join tb4
     on concat(tb3.R_score, tb3.F_score, tb3.M_score) = tb4.RFM_score
 -- order by R 
 
+-- Tinh phan tram khach hang theo tung phan khuc
+-- select 
+--     Segment,
+--     count(CustomerKey) as 'NumberOfCus',
+--     -- sum(count(CustomerKey)) over() as 'Total',
+--     round(cast(count(CustomerKey) as float)*100 / cast(sum(count(CustomerKey)) over() as float), 2) as 'Percent %'
+-- from tb5
+-- group by Segment
+-- order by 3 desc 
 
 
 
--- Xem thong tin bang
-select top(20)*
-from [dbo].[SaleOrders] as so
 
-select 
-    max(so.OrderDate),
-    min(so.OrderDate)
-from [dbo].[SaleOrders] as so 
-
-select * from [dbo].[RankRFM]
 
